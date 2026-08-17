8c598120c6b0c9cd62af7a003601f952d33334c4
pratap9593 <ledangepratap@gmail.com>
2026-08-17 07:30:21 +0530

    new changes

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("This is java program");
         System.out.println("Output of java program");
         System.out.println(new Date());
+        System.out.println("My name is pratap");
     }
 }
